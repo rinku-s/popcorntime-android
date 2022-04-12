56ceb9cb460686c9eaee497f26fb94a8b5e65cd1
rinku-s <99785438+rinku-s@users.noreply.github.com>
2022-04-11 23:54:18 -0600

    Add files via upload

---
+++ helpers/Colors.js
@@ -0,0 +1,22 @@
+/*
+ *  file: Colors.js
+ *  author: Rinku Ansari <raf122@uregina.ca>
+ *  version: 0.1
+ *  date-created: mar-30-2022
+ *  last-modified: apr-01-2022
+ */
+
+export const Colors = {
+   /**
+       * color
+       * Purpose: Define the colors used for the theme in the app
+``*/
+
+  primary : "#51489a",//"#361d32",
+  primaryVariant: "#8078bf",
+  secondary : "#ffc14f", //"#edd2cb",
+  secondaryVariant: "#ffe1aa", // "#f1e8e6",
+  accent: "#ff9f22",
+  backgroundColor: "#f4f6fc",
+  main:"#51489a",
+}
